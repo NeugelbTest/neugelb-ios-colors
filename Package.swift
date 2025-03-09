09d7abf141158537bf3b4f89cb6556d77ca17e45
Matt Moayeri <notron021@gmail.com>
2025-03-09 16:27:49 +0100

    initial commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "NeugelbColors",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "NeugelbColors",
+            targets: ["NeugelbColors"]),
+    ],
+    targets: [
+        .target(
+            name: "NeugelbColors",
+            path: "./Sources",
+            resources: [.process("Resources")]
+        ),
+        .testTarget(
+            name: "NeugelbColorsTests",
+            dependencies: ["NeugelbColors"]
+        ),
+    ]
+)
+
